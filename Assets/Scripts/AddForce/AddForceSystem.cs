f38adcd96dfd502c85307b5d7390bc837fb95103
Ed <edward2.neal@live.uwe.ac.uk>
2022-06-07 16:42:33 +0100

    Added force to sphere

---
+++ Assets/Scripts/AddForce/AddForceSystem.cs
@@ -0,0 +1,29 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Physics.Extensions;
+using Unity.Transforms;
+using Unity.Collections;
+using UnityEngine;
+
+namespace TMG.PhysicsAddForces
+{
+    [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+    public partial class AddForceSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            var deltaTime = Time.DeltaTime;
+            Entities.ForEach((Entity sphere, ref PhysicsVelocity physicsVelocity, ref PhysicsMass physicsMass,
+                in MoveForceData moveForceData) =>
+            {
+                if (Input.GetKey(moveForceData.ForwardInputKey))
+                {
+                    var forceVector = (float3)Vector3.forward * moveForceData.ForceAmount * deltaTime;
+                    physicsVelocity.ApplyLinearImpulse(physicsMass, forceVector);
+
+                }
+            }).Run();
+        }
+    }
+}
---
+++ Assets/Scripts/Projectile/DotProjectile.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DotProjectile : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public float initialVelocity;
+    public KeyCode FireKey;
+    public KeyCode DestroyKey;
+
+    private Entity projectileEntity;
+    private EntityManager entityManager;
+    private BlobAssetStore blobAssetStore;
+    private GameObjectConversionSettings settings;
+    private List<Entity> projectiles;
+    
+
+    void Start()
+    {
+        // using DOTS, a bit of boilerplate to instantiate the entities from a gameobject prefab is required
+        entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        blobAssetStore = new BlobAssetStore();
+        settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, blobAssetStore);
+        // now this line converts the cubePrefab to an Entity inside this script
+        projectileEntity = GameObjectConversionUtility.ConvertGameObjectHierarchy(projectilePrefab, settings);
+
+        projectiles = new List<Entity>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(FireKey))
+        {
+            //Remove other projectiles from the scene
+
+            Entity projectile = entityManager.Instantiate(projectileEntity);
+
+            Translation projectileTranslation = new Translation
+            {
+                Value = new Unity.Mathematics.float3(this.transform.position)
+            };
+            entityManager.SetComponentData(projectile, projectileTranslation);
+
+            projectiles.Add(projectile);
+
+            //Old
+            //var projectileInstance = GameObject.Instantiate(projectile, this.transform.position, Quaternion.identity);
+            //projectileInstance.GetComponent<Rigidbody>().velocity = initialVelocity * this.transform.forward;
+        }
+
+        if (Input.GetKeyDown(DestroyKey))
+        {
+            foreach (Entity ent in projectiles)
+            {
+                entityManager.DestroyEntity(ent);
+
+            }
+            projectiles.Clear();
+        }
+    }
+
+    private void OnDisable()
+    {
+        blobAssetStore.Dispose();
+    }
+
+    private void OnDestroy()
+    {
+        blobAssetStore.Dispose();
+    }
+}
